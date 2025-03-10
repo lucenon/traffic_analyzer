2d6f3b9c1031c61d0415d75bf020e29e56df89e3
lucenon <you@example.com>
2025-03-10 16:20:43 +0300

    add sniffer file

---
+++ sniffer.py
@@ -0,0 +1,29 @@
+from src.scripts import train, config, PacketAnalyzer
+from joblib import load
+import warnings
+import argparse
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+def main(mode, duration=config.DURATION, file_path=None):
+    train.train()
+    model = load(config.MODEL_PATH)
+    analyzer = PacketAnalyzer.PacketAnalyzer(mode, file_record_path=config.FILE_RECORD, model=model)
+    if file_path:
+        analyzer.analyze_file(file_path)
+    else:
+        analyzer.capture(duration)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Packet analyzer')
+
+    parser.add_argument('--duration', type=int, default=10, help='Duration for live packet capturing (default: 10)')
+    parser.add_argument('--file', type=str, help='Path to the file for analysis')
+    parser.add_argument('--file_record', type=str, help='Path to the file for record')
+    parser.add_argument('--analyze', type=bool, default=True, help='Analysis or capture mode')
+
+    args = parser.parse_args()
+
+    main(mode=args.analyze, duration=args.duration, file_path=args.file)
